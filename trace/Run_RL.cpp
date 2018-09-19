421391ba18ad2cf3b4f099111cd0d24b0ef9ef76
Tomohisa Oseko <e165702@ie.u-ryukyu.ac.jp>
2018-09-19 17:41:53 +0900

    Lコースの走行

--- trace/Run_RL.cpp
+++ trace/Run_RL.cpp
@@ -1,7 +1,27 @@
 #include "Run_RL.hpp"
 
 void Run_RL::run_L() {
+    Pointers pt_s(l_Wheel, r_Wheel, c_Sensor, t_Sensor);
 
+    // インスタンスを作成。
+    Tracer tracer(pt_s);
+    Straight straight(pt_s);
+    Turn_oneSide turn_oneSide(pt_s);
+
+    // tracer のテスト
+    tracer.setParam(1);
+    tracer.run(Enums::LEFT, 2500);
+    tracer.setParam(0);
+    tracer.run(Enums::LEFT, 1500);
+    tracer.setParam(1);
+    tracer.run(Enums::LEFT, 1800);
+    tracer.setParam(0);
+    tracer.run(Enums::LEFT, 2000);
+    tracer.setParam(1);
+    tracer.run(Enums::LEFT, 2300);
+    tracer.setParam(0);
+    tracer.run(Enums::LEFT, 1000);
+    tracer.stop();
 }
 
 
