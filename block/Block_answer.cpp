69457c367ee0abe75a7593da1b2ded5d022a3404
M-82s7503 <e165738@ie.u-ryukyu.ac.jp>
2018-10-06 11:46:30 +0900

    【ver0.99】 ● ブロック並べ 最終調整

--- block/Block_answer.cpp
+++ block/Block_answer.cpp
@@ -9,7 +9,8 @@ void Block_answer::run() {
   map.block_jud(solve_num); // 暗号よりブロックの初期配置の計算
 
   /*
-  tai.tocolor(5);
+  */                                                                                                 
+  tai.to_color(5);
   // ブロックまで移動                                                                                                                                                                                     
   map.priority_move(&map.move_x,&map.move_y,map.block); // 優先順位決定                                                                                                                                   
   tai.purpose_move(&map.car_x,&map.car_y,map.move_x,map.move_y,&map.car_degree,map.block,1); // 目的まで移動                                                                                              
@@ -22,7 +23,7 @@ void Block_answer::run() {
   // 次のブロックを探すために一個前に戻る                                                                                                                                                                 
   map.block[map.move_y][map.move_x] = Map_Move::AFTER_MOVE_BLOCK; // マップの更新                                                                                                                         
   tai.back_move(map.car_degree,&map.car_x,&map.car_y); // 一個前の座標に移動      
-  */                                                                                                 
+
   while(1) { 
     // ブロックまで移動                                                                                                                                                                
     map.priority_move(&map.move_x,&map.move_y,map.block); // 優先順位決定                                                                                                                     
@@ -63,7 +64,7 @@ void Block_answer::run() {
 
     
   tai.purpose_move(&map.car_x,&map.car_y,3,2,&map.car_degree,map.block,1); // 目的まで移動                                                                             
-  switch(tai.car_degree){
+  switch(map.car_degree){
   case 90:
     tai.turn(90);
     break;
