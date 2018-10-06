7377f389c28b1b52b060102939dce1dd7b1d9e52
e165761 <e165761@ie.u-ryukyu.ac.jp>
2018-10-06 11:12:52 +0900

    last change

--- block/Block_answer.cpp
+++ block/Block_answer.cpp
@@ -3,29 +3,37 @@
 
 void Block_answer::run() {
   int count = 0; 
-
+  int a[3] = {5,3,4};
+  int k = 0;
   code.Solve(solve_num,code_num);
   map.block_jud(solve_num); // 暗号よりブロックの初期配置の計算
 
+  /*
+  tai.tocolor(5);
   // ブロックまで移動                                                                                                                                                                                     
   map.priority_move(&map.move_x,&map.move_y,map.block); // 優先順位決定                                                                                                                                   
   tai.purpose_move(&map.car_x,&map.car_y,map.move_x,map.move_y,&map.car_degree,map.block,1); // 目的まで移動                                                                                              
   map.block[map.car_y][map.car_x] = Map_Move::NO_EXIST; // マップの更新                                                                                                                                  \
                                                                                                                                                                                                          
-  // ブロックを指定の位置まで移動させる                                                                                                                                                                   
+  // ブロックを指定の位置まで移動させる                                                                                                                                                                  
+  tai.straight(50);
   map.move_point(2,&map.move_x,&map.move_y,map.block); // ブロックの色により向かう場所の決定                                                                                                 
   tai.purpose_move(&map.car_x,&map.car_y,map.move_x,map.move_y,&map.car_degree,map.block,-1); // 目的まで移動                                                                                            
   // 次のブロックを探すために一個前に戻る                                                                                                                                                                 
   map.block[map.move_y][map.move_x] = Map_Move::AFTER_MOVE_BLOCK; // マップの更新                                                                                                                         
   tai.back_move(map.car_degree,&map.car_x,&map.car_y); // 一個前の座標に移動      
-                                                                                                     
+  */                                                                                                 
   while(1) { 
     // ブロックまで移動                                                                                                                                                                
     map.priority_move(&map.move_x,&map.move_y,map.block); // 優先順位決定                                                                                                                     
     tai.purpose_move(&map.car_x,&map.car_y,map.move_x,map.move_y,&map.car_degree,map.block,1); // 目的まで移動                                                                             
     map.block[map.car_y][map.car_x] = Map_Move::NO_EXIST; // マップの更新                                                                                                                                             
-    // ブロックを指定の位置まで移動させる                                                                                                                            
-    map.move_point(color.OutputColor(),&map.move_x,&map.move_y,map.block); // ブロックの色により向かう場所の決定                         
+    // ブロックを指定の位置まで移動させる 
+    if(color.OutputColor() != 0){                                                                                                                           
+      map.move_point(color.OutputColor(),&map.move_x,&map.move_y,map.block); // ブロックの色により向かう場所の決定                         
+    }else{
+      map.move_point(a[k],&map.move_x,&map.move_y,map.block); // ブロックの色により向かう場所の決定                         
+    }
     tai.purpose_move(&map.car_x,&map.car_y,map.move_x,map.move_y,&map.car_degree,map.block,-1); // 目的まで移動                                                                                            
     // 次のブロックを探すために一個前に戻る                                                                                                                                                       
     map.block[map.move_y][map.move_x] = Map_Move::AFTER_MOVE_BLOCK; // マップの更新                                                                                                                       
@@ -35,12 +43,14 @@ void Block_answer::run() {
     int i,j;
     for(i = 0; i < 4; i++){                                                     
       for(j = 0; j < 4; j++){                                                   
-        if(map.block[i][j] == AFTER_MOVE_BLOCK){                                
+        if(map.block[i][j] == Map_Move::AFTER_MOVE_BLOCK){                                
           count++;                                                              
         }                                                                       
       }                                                                         
-    }               
- 
+    }   
+            
+    k++;
+
     if(count == N){
       break;
     }else{
@@ -51,7 +61,7 @@ void Block_answer::run() {
 
   }
     
-
+  tai.purpose_move(&map.car_x,&map.car_y,3,2,&map.car_degree,map.block,1); // 目的まで移動                                                                             
   msg_f("Stopped.", 1);
 
 
--- block/analyze/Color_recognition..cpp
+++ block/analyze/Color_recognition..cpp
@@ -16,32 +16,8 @@ void Color_recognition::GetColor()
 // 判別した色をLCDに表示
 int Color_recognition::OutputColor()
 {
-  box = colorSensor.getColorNumber(); 
-  switch(box){
-  case NONE:
-    msg_f("None...", 1); // LCDに出力
-    break;
-  case BLACK:
-    msg_f("Black!!", 1); // LCDに出力
-    break;
-  case BLUE:
-    msg_f("Blue!!", 1); // LCDに出力
-    break;
-  case GREEN:
-    msg_f("Greeeeeen!!", 1); // LCDに出力
-    break;
-  case YELLOW:
-    msg_f("Yello!!", 1); // LCDに出力
-    break;
-  case RED:
-    msg_f("Red!!", 1); // LCDに出力
-    break;
-  case WHITE:
-    msg_f("White!!", 1); // LCDに出力
-    break;
-  default:
-    break;
-  }
+
+  box = colorSensor.getColorNumber(); // 色を取得
 
   return box;
 
